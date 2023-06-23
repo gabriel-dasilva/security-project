58d4d47079890d17fda6c72e92498ebd217f6481
Tlholo Koma <tlholo@bbd.co.za>
2023-06-23 14:44:12 +0200

    mssql to mysql

--- identityServer/controllers/loginController.js
+++ identityServer/controllers/loginController.js
@@ -4,18 +4,19 @@ const pool = require('../dbconnection/db');
 
 const getUserByUsername = (userName) => {
     return new Promise((resolve, reject) => {
-      pool.query(
-        'SELECT * FROM User WHERE username = ?',
-        [userName],
-        (error, results) => {
-          if (error) {
-            console.error('Error occurred when executing query: ', error);
-            reject(error);
-            return;
-          }
-          resolve(results[0]); 
-        }
-      );
+      pool.request()
+          .input('username', mssql.VarChar,userName)
+          .query(
+            'SELECT * FROM User WHERE username = ?',
+            (error, results) => {
+              if (error) {
+                console.error('Error occurred when executing query: ', error);
+                reject(error);
+                return;
+              }
+              resolve(results[0]); 
+            }
+          );
     });
   };
 
--- identityServer/controllers/registerController.js
+++ identityServer/controllers/registerController.js
@@ -1,19 +1,22 @@
+const mssql = require('mssql');
 const pool = require('../dbconnection/db');
 
 const addUser = (userName, password) => {
     return new Promise((resolve, reject) => {
-      pool.query(
-        'INSERT INTO User (username, password) VALUES (?, ?)',
-        [userName, password],
-        (error) => {
-          if (error) {
-            console.error('Error occurred when executing query: ', error);
-            reject(error);
-            return;
-          }
-          resolve(true);
-        }
-      );
+      pool.request()
+          .input('username', mssql.VarChar,userName)
+          .input('password', mssql.VarChar, password)
+          .query(
+            'INSERT INTO User (username, password) VALUES (?, ?)',
+            (error) => {
+              if (error) {
+                console.error('Error occurred when executing query: ', error);
+                reject(error);
+                return;
+              }
+              resolve(true);
+            }
+        );
     });
   };
 
