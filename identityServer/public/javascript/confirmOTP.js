0bc340b530569043b96269fecb6bc048be0beccd
Gabriel da Silva <gabrield@bbd.co.za>
2023-06-26 18:17:33 +0200

    Create resource server only branch

--- identityServer/public/javascript/confirmOTP.js
+++
@@ -1,9 +0,0 @@
-fetch('/confirmOTP')
-  .then(response => response.json())
-  .then(data => {
-    const emailElement = document.getElementById('email');
-    emailElement.innerText = data.maskedEmail;
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
