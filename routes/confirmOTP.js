e47ed6708befe66c32ac5bdbecaf327c85411013
Gabriel da Silva <gabrield@bbd.co.za>
2023-06-29 20:26:16 +0200

    Revert to temp fix of last stable

--- routes/confirmOTP.js
+++ routes/confirmOTP.js
@@ -47,39 +47,19 @@ router.get('/', (req, res) => {
           const expiryDate = new Date(Date.now() + fifteenMinutes);
       
           user.token = token;
-            
-            // Include token and username in the response headers
-            res.setHeader('Authorization', `Bearer ${token}`);
-            res.setHeader('X-Username', username);
-            
-            // Redirect to the resource server with 307 status code
-            res.redirect(307, 'https://abrzgdhmf3.us-east-1.awsapprunner.com/blackjack');
-          /*res.cookie('token', token, {
+          res.cookie('token', token, {
             expires: expiryDate,
             httpOnly: true,
             secure: true,
-            sameSite: 'none',
-            path: '/',
-            domain: 'abrzgdhmf3.us-east-1.awsapprunner.com'         
+            sameSite: 'none'          
           });
           res.cookie('username', user.username, {
             expires: expiryDate,
             httpOnly: false,
             secure: true,
-            sameSite: 'none',
-            path: '/',
-            domain: 'abrzgdhmf3.us-east-1.awsapprunner.com'           
-          });*/
-
-          /*res.writeHead(302, {
-            'Location': 'https://abrzgdhmf3.us-east-1.awsapprunner.com/blackjack',
-            'Set-Cookie': [
-              `token=${token}; SameSite=None; Secure`,
-              `username=${user.username}; SameSite=None; Secure`
-            ]
+            sameSite: 'none'          
           });
-          res.end();
-          //res.redirect('https://abrzgdhmf3.us-east-1.awsapprunner.com/blackjack');*/
+          res.redirect('https://abrzgdhmf3.us-east-1.awsapprunner.com/blackjack');
     }else{
         console.log("failed to log in!");
     }
